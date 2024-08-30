acad60b8401c43ebb13e5d0ba1162d1c6b4f9673
Faisal Abid <ifaisalabid1@gmail.com>
2024-08-30 20:43:41 +0530

    Add SITE_URL variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,26 @@
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import { remarkReadingTime } from "./remark-reading-time.mjs";
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import { remarkReadingTime } from './remark-reading-time.mjs'
+import { SITE_URL } from '$lib/const.ts'
 
 export default defineConfig({
-  site: "https://techiefaisal.com/",
-  trailingSlash: "always",
+  site: { SITE_URL },
+  trailingSlash: 'always',
   prefetch: {
-    defaultStrategy: "viewport",
+    defaultStrategy: 'viewport'
   },
 
   integrations: [
     svelte(),
     tailwind({
-      applyBaseStyles: false,
+      applyBaseStyles: false
     }),
-    sitemap(),
+    sitemap()
   ],
 
   markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
-});
+    remarkPlugins: [remarkReadingTime]
+  }
+})
