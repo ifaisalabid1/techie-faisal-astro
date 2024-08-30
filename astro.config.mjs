e7c106ac927ca76ecd19b3e26c8f27b29c5f7e25
Faisal Abid <ifaisalabid1@gmail.com>
2024-08-30 20:49:51 +0530

    Remove site_url variable from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import { remarkReadingTime } from './remark-reading-time.mjs'
-import { SITE_URL } from '$lib/const.ts'
 
 export default defineConfig({
-  site: { SITE_URL },
+  site: 'https://techiefaisal.com/',
   trailingSlash: 'always',
   prefetch: {
     defaultStrategy: 'viewport'
