fc3d371e062f8f08009064f0aba8f5840ddf31cd
Faisal Abid <ifaisalabid1@gmail.com>
2024-09-08 09:48:24 +0530

    capitalize title of tag page

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -115,7 +115,9 @@
 
         <button on:click={handleNav} class="cursor-pointer" aria-label="Menu Icon">
           <span class="text-4xl">
-            <Icon icon={nav ? 'material-symbols:close' : 'material-symbols:menu'} />
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z" />
+            </svg>
           </span>
         </button>
       </div>
