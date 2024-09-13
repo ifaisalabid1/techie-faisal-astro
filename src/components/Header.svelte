ce67ce1946a882ca283ee4f9e32b4045d545434e
Faisal Abid <ifaisalabid1@gmail.com>
2024-09-13 20:29:47 +0530

    Remove demo article

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -115,9 +115,7 @@
 
         <button on:click={handleNav} class="cursor-pointer" aria-label="Menu Icon">
           <span class="text-4xl">
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-              <path fill="currentColor" d="M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z" />
-            </svg>
+            <Icon icon="material-symbols:menu" />
           </span>
         </button>
       </div>
