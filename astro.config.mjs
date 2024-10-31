d2de5e734f22af1f17df11c7964381ca08d3c717
Faisal Abid <ifaisalabid1@gmail.com>
2024-10-31 22:53:24 +0530

    Apply trailing slash always

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://techiefaisal.com',
-  trailingSlash: 'never',
+  trailingSlash: 'always',
   prefetch: {
     defaultStrategy: 'viewport'
   },
